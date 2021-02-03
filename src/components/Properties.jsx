132f0fb0e4205aa9aaec4e97ddfef2e9b8970606
Malo Pivert <pivertmalo@gmail.com>
2021-02-03 18:57:17 +0100

    properties components axios get call is working, no css yet

--- src/components/Properties.jsx
+++ src/components/Properties.jsx
@@ -1,5 +1,28 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 function Properties() {
-  return <h1>Properties</h1>;
+  const [properties, setProperties] = useState([
+    {
+      Title: 'Something',
+      Description: 'went really wrong',
+      Lieux: 'on the server',
+    },
+  ]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3090/api/v1/properties`)
+      .then((res) => setProperties(res.data));
+  }, []);
+
+  return properties.map((e) => (
+    <ul>
+      <li>{e.Title}</li>
+      <li>{e.Description}</li>
+      <li>{e.Lieux}</li>
+    </ul>
+  ));
 }
 
 export default Properties;
--- src/env.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config();
-
-/* eslint-disable import/prefer-default-export */
-export const URL_API =
-  process.env.REACT_APP_API_URL || 'http://localhost:5000/api/v1/';
